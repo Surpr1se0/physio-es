887d83d877a01fde83f4e9ea3a46c304c1290f75
Sequeira12 <brunomsequeira@hotmail.com>
2024-05-10 17:05:04 +0100

    new cenas

--- ebdjango/lambda_funcs/pagamento.py
+++ ebdjango/lambda_funcs/pagamento.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+import psycopg2
 
 def lambda_handler(event, context):
 
@@ -8,6 +9,13 @@ def lambda_handler(event, context):
   payment_amount = event.get('payment_amount')
   client = boto3.client('dynamodb', region_name='us-east-1')
 
+  conn = psycopg2.connect(
+        database="postgres", 
+        user="postgres", 
+        password="postgres", 
+        host="database-1.cgakkke3mf3d.us-east-1.rds.amazonaws.com", port="5432")
+    
+  cur = conn.cursor()
   # Perform idempotent payment logic
   try:
 
@@ -32,7 +40,10 @@ def lambda_handler(event, context):
         
     elif item['status']['S'] == 'Payment in progress':
       item['status'] = {'S':'schedule'}
-
+      cur.execute("select money from edjango_user where id=%d",user_id)
+      rows = cur.fetchall()
+      user_money = rows[0] - payment_amount
+      cur.execute("UPDATE ebdjango_user set money = %d where id=%d",user_money,user_id)
       client.put_item(TableName='appointments1',Item=item)
 
       return {
--- ebdjango/second-frontend/src/index.js
+++ ebdjango/second-frontend/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import AdminPage from './page/AdminPage';
+
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/adminpage' element={<AdminPage />} />
+      <Route path="/" element={<Navigate to="/adminpage" />} />
     </Routes>
   </BrowserRouter>
 
